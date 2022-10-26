1f7d1c26881e339ff0ae4b587d3acacd1595a1b3
pain4metoo <pain4metoo@gmail.com>
2022-10-26 22:17:39 +0200

    refactor: uniform naming style for selectors in the style.css and app.js to rule 3.1 in the html-and-css.md

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ var createNewTaskElement = function (taskString) {
     editInput.className = "task";
 
     editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className = "edit";
+    editButton.className = "main-edit-btn";
 
-    deleteButton.className = "delete";
+    deleteButton.className = "main-delete-btn";
     deleteButtonImg.src = './remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -83,7 +83,7 @@ var editTask = function () {
     var listItem = this.parentNode;
     var editInput = listItem.querySelector('input[type=text]');
     var label = listItem.querySelector("label");
-    var editBtn = listItem.querySelector(".edit");
+    var editBtn = listItem.querySelector(".main-edit-btn");
     var containsClass = listItem.classList.contains("main-edit-mode");
     //If class of the parent is .editmode
     if (containsClass) {
@@ -154,10 +154,10 @@ var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
     console.log("bind list item events");
     //select ListItems children
     var checkBox = taskListItem.querySelector("input[type=checkbox]");
-    var editButton = taskListItem.querySelector("button.edit");
-    var deleteButton = taskListItem.querySelector("button.delete");
+    var editButton = taskListItem.querySelector(".main-edit-btn");
+    var deleteButton = taskListItem.querySelector(".main-delete-btn");
 
-
+    console.log(taskListItem)
     //Bind editTask to edit button.
     editButton.onclick = editTask;
     //Bind deleteTask to delete button.
